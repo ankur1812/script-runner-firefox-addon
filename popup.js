44fd1e3e9c3d845e90a9a274eb81279282a60e57
ankur1812 <ankur.anmol1992@gmail.com>
2024-10-06 01:50:36 +0200

    add enable-checkboxes for js and css, temporarily  disable global scripts

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 const dqs = (s) => document.querySelector(s);
+const dqsA = (s) => Array.from(document.querySelectorAll(s));
 const ael = (s, e, fn, z=false) => dqs(s).addEventListener(e, fn, z)
 const config = {url: ''};
+
 
 const injectNow = () => {
     const injectCode = {
@@ -11,19 +13,43 @@ const injectNow = () => {
             jsEnabled: dqs('#enable-js').checked,
         }
     }
-    let globalCode = {
-        globalCode: {
-            cssGlobal:dqs('#css-box-global').value,
-            jsGlobal: dqs('#js-box-global').value,
-            cssGlobalEnabled:dqs('#enable-css-global').checked,
-            jsGlobalEnabled: dqs('#enable-js-global').checked,    
-        }
-    }
+    // let globalCode = {
+    //     globalCode: {
+    //         cssGlobal:dqs('#css-box-global').value,
+    //         jsGlobal: dqs('#js-box-global').value,
+    //         cssGlobalEnabled:dqs('#enable-css-global').checked,
+    //         jsGlobalEnabled: dqs('#enable-js-global').checked,
+    //     }
+    // }
+
     browser.storage.local.set(injectCode);
-    browser.storage.local.set(globalCode);
+    // browser.storage.local.set(globalCode);
     dqs('#success-msg').innerHTML =  "JS/CSS added. Refresh to see changes!"
 }
-dqs('#save-btn').addEventListener('click', injectNow)   
+
+dqs('#save-btn').addEventListener('click', injectNow)
+
+const toggleAccordion = (containerDivClass) => {
+    let isHidden = dqs(`.${containerDivClass} textarea`)?.classList.contains('hidden');
+    dqs(`.${containerDivClass} textarea`)?.classList.toggle('hidden');
+    if (dqs(`.${containerDivClass} .chevron`)) {
+        dqs(`.${containerDivClass} .chevron`).classList.toggle('collapsed', !isHidden)
+    }
+
+
+    dqsA('#local-scripts textarea').forEach( ta => {
+        let parentClass = ta.parentElement.className
+        let currentDiv = parentClass == containerDivClass;
+        if(!currentDiv){
+            ta.classList.toggle('hidden', true)
+            dqs(`.${parentClass} .chevron`)?.classList.toggle('collapsed', true)
+        }
+    });
+}
+
+dqsA('.accordion-header').forEach ( h => {
+    h.addEventListener('click', (e) => e.target.tagName != 'INPUT' && toggleAccordion(h.parentElement.className))
+})
 
 browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
     let tab = tabs[0];
@@ -34,7 +60,10 @@ browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
     function toggleView(a,b) {
         dqs('#local-scripts').style.display = a
         dqs('#global-scripts').style.display = b
+        dqs('#show-local').classList.toggle('active', a == 'block')
+        dqs('#show-global').classList.toggle('active', b == 'block')
     }
+
     ael('#show-local','click', (e)=>{toggleView('block', 'none')});
     ael('#show-global','click', (e)=>{toggleView('none', 'block')});
 
